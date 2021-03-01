a9c2b6edae085b7496c5ce3056d039355d968421
jovair <jvr_ribeirolive.com>
2021-03-01 15:46:52 -0300

    Finalizado o botão igual sem erro

--- assets/js/controller/calcController.js
+++ assets/js/controller/calcController.js
@@ -1,5 +1,7 @@
 class calcController {
   constructor() {
+    this._lastOperator = "";
+    this._lastNumber = "";
     this._operation = [];
     this._displayCalcEl = document.querySelector("#display");
     this.initButtonsEvents();
@@ -58,12 +60,24 @@ class calcController {
     }
   }
 
+  getResult() {
+    return eval(this._operation.join(""));
+  }
+
   calc() {
     let last = "";
+    this._lastOperator = this.getLastItem();
+    if (this._operation.length < 3) {
+      let firtsItem = this._operation[0];
+      this._operation = [firtsItem, this._lastOperator, this._lastNumber];
+    }
     if (this._operation.length > 3) {
       last = this._operation.pop();
+      this._lastNumber = this.getResult();
+    } else if (this._operation.length == 3) {
+      this._lastNumber = this.getLastItem(false);
     }
-    let result = eval(this._operation.join(""));
+    let result = this.getResult();
     if (last == "%") {
       result /= 100;
       this._operation = [result];
@@ -73,14 +87,22 @@ class calcController {
     this.setLastNumberToDisplay();
   }
 
-  setLastNumberToDisplay() {
-    let lastNumber;
+  getLastItem(isOperator = true) {
+    let lastItem;
     for (let i = this._operation.length - 1; i >= 0; i--) {
-      if (!this.isOperator(this._operation[i])) {
-        lastNumber = this._operation[i];
+      if (this.isOperator(this._operation[i]) == isOperator) {
+        lastItem = this._operation[i];
         break;
       }
     }
+    if (!lastItem) {
+      lastItem = isOperator ? this._lastOperator : this._lastNumber;
+    }
+    return lastItem;
+  }
+
+  setLastNumberToDisplay() {
+    let lastNumber = this.getLastItem(false);
     if (!lastNumber) lastNumber = 0;
     this.displayCalc = lastNumber;
   }
